576d063c4f71bd9492111e583cc4636348fa9b7b
niamin-niaman <niamin.niaman@gmail.com>
2024-09-26 18:42:34 +0000

    fix

--- exercise/ex1.c
+++ exercise/ex1.c
@@ -1,21 +1,24 @@
+#include <stdio.h>
+
 // find min , max
 
 int findMin(int arr[], int size)
 {
 
-    return 0;
 }
 
 int findMax(int arr[], int size)
 {
 
-    return 0;
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // random , unsorted
+    int arr[]  = {10, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+
+
     int size = sizeof(arr) / sizeof(arr[0]);
 
     int min = findMin(arr, size);
